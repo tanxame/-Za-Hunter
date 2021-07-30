fe521e7e7a396e139918da055b9815cbd9ecabb9
Amelia <atan@ucls.uchicago.edu>
2021-07-30 17:38:39 -0500

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Franklin Tan on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
